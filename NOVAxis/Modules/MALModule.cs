aa36c9554f7a775efac6282e8fe29ccac494691a
Kryštof Novosad <novosadkry@seznam.cz>
2019-08-16 22:02:34 +0200

    Improve MALModule module functionality

--- NOVAxis/Modules/MALModule.cs
+++ NOVAxis/Modules/MALModule.cs
@@ -21,68 +21,161 @@ namespace NOVAxis.Modules
     [RequireUserPermission(GuildPermission.CreateInstantInvite)]
     public class MALModule : InteractiveBase<SocketCommandContext>
     {
-        private abstract class MALJson
+        private static class MALJson
         {
-            public const string API = "https://api.jikan.moe/v3/search/{0}?q={1}&limit={2}";
+            public const string API = "https://api.jikan.moe/v3/{0}";
 
             public abstract class MALResult
             {
+                public class Info
+                {
+                    public class Genre
+                    {
+                        public int mal_id { get; set; }
+                        public string type { get; set; }
+                        public string name { get; set; }
+                        public string url { get; set; }
+                    }
+
+                    public string title_english { get; set; }
+                    public List<string> title_synonyms { get; set; }
+                    public string title_japanese { get; set; }
+                    public string status { get; set; }
+                    public string type { get; set; }
+                    public string rating { get; set; }
+                    public int? rank { get; set; }
+                    public List<Genre> genres { get; set; }
+                }
+
                 public class Anime : MALResult
                 {
                     public bool airing { get; set; }
+                    public string aired
+                    {
+                        get => base.published;
+                    }
+
                     public int episodes { get; set; }
                     public string rated { get; set; }
+
+                    protected override string api
+                    {
+                        get => string.Format(API, "anime/{0}");
+                    }
                 }
 
                 public class Manga : MALResult
                 {
                     public bool publishing { get; set; }
+                    public new string published
+                    {
+                        get => base.published;
+                    }
+
                     public int chapters { get; set; }
                     public int volumes { get; set; }
+
+                    protected override string api
+                    {
+                        get => string.Format(API, "manga/{0}");
+                    }
                 }
 
+                protected virtual string published
+                {
+                    get => $"from `{start_date?.ToShortDateString() ?? "?"}`\n" +
+                        $"to `{end_date?.ToShortDateString() ?? "?"}`";
+                }
+
+                public virtual async Task<Info> GetInfo()
+                {
+                    string api = string.Format(this.api, mal_id);
+                    api = Uri.EscapeUriString(api);
+
+                    using (WebClient client = new WebClient() { Encoding = Encoding.UTF8 })
+                    {
+                        Task<string> result = client.DownloadStringTaskAsync(api);
+                        return JObject.Parse(await result).ToObject<Info>();
+                    }
+                }
+
+                protected abstract string api { get; }
+
+                public Info info { get; set; }
                 public int mal_id { get; set; }
                 public string url { get; set; }
                 public string image_url { get; set; }
                 public string title { get; set; }
                 public string synopsis { get; set; }
                 public string type { get; set; }
-                public double score { get; set; }
-                public DateTime start_date { get; set; }
+                public float score { get; set; }
+                public DateTime? start_date { get; set; }
                 public DateTime? end_date { get; set; }
                 public int members { get; set; }
             }
-        }
 
-        private class MALJson<T> : MALJson, IEnumerable<T> where T : MALJson.MALResult
-        {
-            public readonly string json;
-
-            public MALJson(string json)
-            {
-                this.json = json;
-            }
-
-            IEnumerable<T> Convert()
+            public static List<T> Get<T>(string json) where T : MALResult
             {
                 JObject mainObject = JObject.Parse(json);
                 JArray dataArray = (JArray)mainObject["results"];
 
-                return dataArray.ToObject<IEnumerable<T>>();
-            }
-
-            public IEnumerator<T> GetEnumerator()
-            {
-                return Convert().GetEnumerator();
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return GetEnumerator();
+                return dataArray.ToObject<List<T>>();
             }
         }
 
         public InteractiveService InteractiveService { get; set; }
+
+        private async Task ShowResults<T>(List<T> results) where T : MALJson.MALResult
+        {
+            if (results.Count <= 0)
+                throw new Exception("Výsledek databáze neobsahuje žádný prvek");
+
+            EmbedFieldBuilder[] embedFields = new EmbedFieldBuilder[results.Count];
+
+            for (int i = 0; i < results.Count; i++)
+            {
+                T element = results[i];
+
+                if (element is MALJson.MALResult.Anime)
+                {
+                    var mal = element as MALJson.MALResult.Anime;
+
+                    embedFields[i] = new EmbedFieldBuilder
+                    {
+                        Name = $"**[{i + 1}]** {mal.title}",
+                        IsInline = false,
+                        Value = $"*({mal.start_date?.Year.ToString() ?? "?"})* " +
+                        $"{mal.type} : {(mal.airing ? "Airing" : "Finished")} " +
+                        $"| Episodes: {mal.episodes} " +
+                        $"| Score: {mal.score}/10"
+                    };
+                }
+
+                else if (element is MALJson.MALResult.Manga)
+                {
+                    var mal = element as MALJson.MALResult.Manga;
+
+                    embedFields[i] = new EmbedFieldBuilder
+                    {
+                        Name = $"**[{i + 1}]** {mal.title}",
+                        IsInline = false,
+                        Value = $"*({mal.start_date?.Year.ToString() ?? "?"})* " +
+                        $"{mal.type} : {(mal.publishing ? "Publishing" : "Finished")} " +
+                        $"| Volumes: {mal.volumes} " +
+                        $"| Score: {mal.score}/10"
+                    };
+                }
+
+                else
+                    throw new NotImplementedException();
+            }
+
+            await ReplyAsync(embed: new EmbedBuilder()
+                .WithTitle($"**Výsledek databáze serveru MyAnimeList.net** (počet výsledků: {results.Count})")
+                .WithDescription("(Proveďte výběr pro více informací)")
+                .WithColor(150, 0, 150)
+                .WithFields(embedFields).Build());
+        }
 
         [Command("anime"), Summary("Searches for anime in MyAnimeList.net database")]
         public async Task SearchAnime(string name, ushort limit = 5)
@@ -92,12 +185,23 @@ namespace NOVAxis.Modules
                 await ReplyAsync(embed: new EmbedBuilder()
                     .WithColor(220, 20, 60)
                     .WithDescription($"(Argument nesmí být menší nebo roven nule)")
-                    .WithTitle($"Zajímalo by mě, co zjistíš z nula a méně prvků...").Build());
+                    .WithTitle($"Mé jádro nebylo schopno příjmout daný prvek").Build());
 
                 return;
             }
 
-            string api = string.Format(MALJson.API, "anime", name, limit);
+            if (name.Length < 3)
+            {
+                await ReplyAsync(embed: new EmbedBuilder()
+                    .WithColor(220, 20, 60)
+                    .WithDescription($"(Argument musí být delší než tři znaky)")
+                    .WithTitle($"Mé jádro nebylo schopno příjmout daný prvek").Build());
+
+                return;
+            }
+
+            string api = string.Format(MALJson.API, "search/anime?q={0}&limit={1}");
+            api = string.Format(api, name, limit);
             api = Uri.EscapeUriString(api);
 
             using (WebClient client = new WebClient() { Encoding = Encoding.UTF8 })
@@ -106,33 +210,10 @@ namespace NOVAxis.Modules
                 {
                     string result = await client.DownloadStringTaskAsync(api);
 
-                    List<MALJson.MALResult.Anime> collection = new MALJson<MALJson.MALResult.Anime>(result).ToList();
+                    List<MALJson.MALResult.Anime> collection = 
+                        MALJson.Get<MALJson.MALResult.Anime>(result);
 
-                    if (collection.Count <= 0)
-                        throw new Exception("Výsledek databáze neobsahuje žádný prvek");
-
-                    EmbedFieldBuilder[] embedFields = new EmbedFieldBuilder[collection.Count];
-
-                    for (int i = 0; i < collection.Count; i++)
-                    {
-                        MALJson.MALResult.Anime mal = collection[i];
-
-                        embedFields[i] = new EmbedFieldBuilder
-                        {
-                            Name = $"[{i + 1}] {mal.title}",
-                            IsInline = false,
-                            Value = $"{mal.type} : {(mal.airing ? "Airing" : "Finished")} " +
-                            $"| {mal.start_date.ToShortDateString()} to {mal.end_date?.ToShortDateString() ?? "?"} " +
-                            $"| Episodes: {mal.episodes}"
-                        };
-                    }
-
-                    await ReplyAsync(embed: new EmbedBuilder()
-                        .WithTitle($"**Výsledek databáze serveru MyAnimeList.net** (počet výsledků: {collection.Count})")
-                        .WithDescription("(Proveďte výběr pro více informací)")
-                        .WithColor(150, 0, 150)
-                        .WithFields(embedFields).Build());
-
+                    await ShowResults(collection);
                     var input = await NextMessageAsync();
 
                     try
@@ -145,10 +226,11 @@ namespace NOVAxis.Modules
                                     throw new Exception("Neplatný výběr");
 
                                 MALJson.MALResult.Anime mal = collection[select - 1];
+                                mal.info = await mal.GetInfo();
 
                                 await ReplyAsync(embed: new EmbedBuilder()
-                                    .WithTitle(mal.title)
-                                    .WithUrl(mal.url)
+                                    .WithAuthor($"{mal.title} (#{mal.info.rank?.ToString() ?? "?"})", url: mal.url)
+                                    .WithTitle(mal.info.title_japanese)
                                     .WithThumbnailUrl(mal.image_url)
                                     .WithDescription(mal.synopsis)
                                     .WithColor(150, 0, 150)
@@ -162,8 +244,15 @@ namespace NOVAxis.Modules
 
                                         new EmbedFieldBuilder
                                         {
+                                            Name = "Status:",
+                                            Value = mal.info.status,
+                                            IsInline = true
+                                        },
+
+                                        new EmbedFieldBuilder
+                                        {
                                             Name = "Aired:",
-                                            Value = $"`{mal.start_date.ToShortDateString()} to {mal.end_date?.ToShortDateString() ?? "?"}`",
+                                            Value = $"{mal.aired}",
                                             IsInline = true
                                         },
 
@@ -179,6 +268,20 @@ namespace NOVAxis.Modules
                                             Name = "Score:",
                                             Value = $"{mal.score}/10",
                                             IsInline = true
+                                        },                                       
+
+                                        new EmbedFieldBuilder
+                                        {
+                                            Name = "Rating:",
+                                            Value = mal.info.rating,
+                                            IsInline = true
+                                        },
+
+                                        new EmbedFieldBuilder
+                                        {
+                                            Name = "Genre:",
+                                            Value = string.Join(", ", mal.info.genres.Select(x => x.name)),
+                                            IsInline = true
                                         }
                                 ).Build());
                             }
@@ -193,7 +296,9 @@ namespace NOVAxis.Modules
 
                     catch (Exception e)
                     {
-                        await ReplyAsync(embed: new EmbedBuilder()
+                        await InteractiveService.ReplyAndDeleteAsync(Context, null,
+                            timeout: new TimeSpan(0, 0, 5),
+                            embed: new EmbedBuilder()
                             .WithColor(220, 20, 60)
                             .WithDescription($"({e.Message})")
                             .WithTitle($"Mé jádro přerušilo čekání na lidský vstup").Build());
@@ -233,7 +338,8 @@ namespace NOVAxis.Modules
                 return;
             }
 
-            string api = string.Format(MALJson.API, "manga", name, limit);
+            string api = string.Format(MALJson.API, "search/manga?q={0}&limit={1}");
+            api = string.Format(api, name, limit);
             api = Uri.EscapeUriString(api);
 
             using (WebClient client = new WebClient() { Encoding = Encoding.UTF8 })
@@ -242,33 +348,10 @@ namespace NOVAxis.Modules
                 {
                     string result = await client.DownloadStringTaskAsync(api);
 
-                    List<MALJson.MALResult.Manga> collection = new MALJson<MALJson.MALResult.Manga>(result).ToList();
+                    List<MALJson.MALResult.Manga> collection = 
+                        MALJson.Get<MALJson.MALResult.Manga>(result);
 
-                    if (collection.Count <= 0)
-                        throw new Exception("Výsledek databáze neobsahuje žádný prvek");
-
-                    EmbedFieldBuilder[] embedFields = new EmbedFieldBuilder[collection.Count];
-
-                    for (int i = 0; i < collection.Count; i++)
-                    {
-                        MALJson.MALResult.Manga mal = collection[i];
-
-                        embedFields[i] = new EmbedFieldBuilder
-                        {
-                            Name = $"[{i + 1}] {mal.title}",
-                            IsInline = false,
-                            Value = $"{mal.type} : {(mal.publishing ? "Publishing" : "Finished")} " +
-                            $"| {mal.start_date.ToShortDateString()} to {mal.end_date?.ToShortDateString() ?? "?"} " +
-                            $"| Volumes: {mal.volumes}"
-                        };
-                    }
-
-                    await ReplyAsync(embed: new EmbedBuilder()
-                        .WithTitle($"**Výsledek databáze serveru MyAnimeList.net** (počet výsledků: {collection.Count})")
-                        .WithDescription("(Proveďte výběr pro více informací)")
-                        .WithColor(150, 0, 150)
-                        .WithFields(embedFields).Build());
-
+                    await ShowResults(collection);
                     var input = await NextMessageAsync();
 
                     try
@@ -281,10 +364,11 @@ namespace NOVAxis.Modules
                                     throw new Exception("Neplatný výběr");
 
                                 MALJson.MALResult.Manga mal = collection[select - 1];
+                                mal.info = await mal.GetInfo();
 
                                 await ReplyAsync(embed: new EmbedBuilder()
-                                    .WithTitle(mal.title)
-                                    .WithUrl(mal.url)
+                                    .WithAuthor($"{mal.title} (#{mal.info.rank?.ToString() ?? "?"})", url: mal.url)
+                                    .WithTitle(mal.info.title_japanese)
                                     .WithThumbnailUrl(mal.image_url)
                                     .WithDescription(mal.synopsis)
                                     .WithColor(150, 0, 150)
@@ -298,8 +382,22 @@ namespace NOVAxis.Modules
 
                                         new EmbedFieldBuilder
                                         {
-                                            Name = "Aired:",
-                                            Value = $"`{mal.start_date.ToShortDateString()} to {mal.end_date?.ToShortDateString() ?? "?"}`",
+                                            Name = "Status:",
+                                            Value = mal.info.status,
+                                            IsInline = true
+                                        },
+
+                                        new EmbedFieldBuilder
+                                        {
+                                            Name = "Published:",
+                                            Value = $"{mal.published}",
+                                            IsInline = true
+                                        },
+
+                                        new EmbedFieldBuilder
+                                        {
+                                            Name = "Score:",
+                                            Value = $"{mal.score}/10",
                                             IsInline = true
                                         },
 
@@ -319,8 +417,8 @@ namespace NOVAxis.Modules
 
                                         new EmbedFieldBuilder
                                         {
-                                            Name = "Score:",
-                                            Value = $"{mal.score}/10",
+                                            Name = "Genre:",
+                                            Value = string.Join(", ", mal.info.genres.Select(x => x.name)),
                                             IsInline = true
                                         }
                                 ).Build());
@@ -336,7 +434,9 @@ namespace NOVAxis.Modules
 
                     catch (Exception e)
                     {
-                        await ReplyAsync(embed: new EmbedBuilder()
+                        await InteractiveService.ReplyAndDeleteAsync(Context, null, 
+                            timeout: new TimeSpan(0, 0, 5),
+                            embed: new EmbedBuilder()
                             .WithColor(220, 20, 60)
                             .WithDescription($"({e.Message})")
                             .WithTitle($"Mé jádro přerušilo čekání na lidský vstup").Build());
