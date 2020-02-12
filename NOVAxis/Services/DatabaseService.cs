e36b76b471307484280614f0dee63047ed304e7e
Kryštof Novosad <novosadkry@seznam.cz>
2020-02-12 18:39:38 +0100

    Add DatabaseService service Add PrefixService service
    Add MySql.Data libraries

---
+++ NOVAxis/Services/DatabaseService.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+
+namespace NOVAxis.Services
+{
+    public class DatabaseService
+    {
+        private ProgramConfig.DatabaseObject config;
+
+        private string ConnectionString
+        {
+            get => string.Format("Data Source={0},{1};Initial Catalog={2};User ID={3};Password={4}", 
+                config.DbHost,
+                config.DbPort,
+                config.DbName, 
+                config.DbUsername,
+                config.DbPassword);
+        }
+
+        public DatabaseService(ProgramConfig config)
+        {
+            this.config = config.Database;
+        }
+
+        public async Task<object> GetValue(string query, params MySqlParameter[] arg)
+        {
+            object result = null;
+
+            using (MySqlConnection sqlc = new MySqlConnection(ConnectionString))
+            {
+                await sqlc.OpenAsync();
+
+                MySqlCommand command = sqlc.CreateCommand();
+                command.CommandText = query;
+                command.Parameters.AddRange(arg);
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            result = reader.GetValue(0);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public async Task<object[]> GetValues(string query, params MySqlParameter[] arg)
+        {
+            object[] result = null;
+
+            using (MySqlConnection sqlc = new MySqlConnection(ConnectionString))
+            {
+                await sqlc.OpenAsync();
+
+                MySqlCommand command = sqlc.CreateCommand();
+                command.CommandText = query;
+                command.Parameters.AddRange(arg);
+
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            reader.GetValues(result);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public async Task Execute(string query, params MySqlParameter[] arg)
+        {
+            using (MySqlConnection sqlc = new MySqlConnection(ConnectionString))
+            {
+                await sqlc.OpenAsync();
+
+                MySqlCommand command = sqlc.CreateCommand();
+                command.CommandText = query;
+                command.Parameters.AddRange(arg);
+
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+    }
+}
---
+++ NOVAxis/Services/PrefixService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+
+namespace NOVAxis.Services
+{
+    class PrefixService
+    {
+        private static Dictionary<ulong, string> cache = new Dictionary<ulong, string>();
+
+        private DatabaseService db;
+
+        public PrefixService(ProgramConfig config)
+        {
+            db = new DatabaseService(config);
+        }
+
+        public async Task<string> GetPrefix(ulong id)
+        {
+            if (!cache.ContainsKey(id))
+                cache[id] = (string) await db.GetValue("SELECT Prefix FROM Guilds WHERE Id=@id", 
+                    new MySqlParameter("id", id));
+
+            return cache[id];
+        }
+
+        public async Task SetPrefix(ulong id, string prefix)
+        {
+            await db.Execute("REPLACE INTO Guilds (Id, Prefix) VALUES (@id, @prefix)",
+                new MySqlParameter("id", id),
+                new MySqlParameter("prefix", prefix));
+
+            cache[id] = prefix;
+        }
+    }
+}
