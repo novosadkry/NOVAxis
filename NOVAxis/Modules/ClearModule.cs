ae106a819439a4d4c42a79b6f79ff4551dfebf33
Kryštof Novosad <novosadkry@seznam.cz>
2020-11-03 23:37:23 +0100

    Delete invocation message along with other messages to keep up with the rate-limit

--- NOVAxis/Modules/ClearModule.cs
+++ NOVAxis/Modules/ClearModule.cs
@@ -18,19 +18,29 @@ namespace NOVAxis.Modules
         {
             try
             {
-                await Context.Message.DeleteAsync();
+                if (numberOfMessages > 100)
+                {
+                    await ReplyAsync(embed: new EmbedBuilder()
+                        .WithColor(220, 20, 60)
+                        .WithDescription("(Maximální povolený počet zpráv je 100)")
+                        .WithTitle("Mé jádro nebylo schopno vymazat daný počet zpráv").Build());
 
-                var messages = (await Context.Channel.GetMessagesAsync(numberOfMessages).FlattenAsync()).ToList();
+                    return;
+                }
 
-                // Removes messages older than 14 days due to Discord API limitations
-                messages.RemoveAll(m => DateTime.UtcNow - m.Timestamp.UtcDateTime > TimeSpan.FromDays(14));
+                var messages = (
+                    from m in await Context.Channel.GetMessagesAsync(numberOfMessages + 1).FlattenAsync() // Add 1 to also count the user's invocation message
+                    where DateTime.UtcNow - m.Timestamp.UtcDateTime < TimeSpan.FromDays(14)                           // Removes messages older than 14 days due to Discord API limitations
+                    select m).ToList();
 
                 await ((ITextChannel)Context.Channel).DeleteMessagesAsync(messages);
 
-                await ReplyAndDeleteAsync(null, embed: new EmbedBuilder()
-                    .WithColor(52, 231, 231)
-                    .WithTitle($"Mé jádro úspěšně vymazalo z existence **{messages.Count}** zpráv" +
-                        (messages.Count == 1 ? "u" : Enumerable.Range(1, 4).Contains(messages.Count) ? "y" : "")).Build(),
+                int count = messages.Count - 1;
+                await ReplyAndDeleteAsync(null,
+                    embed: new EmbedBuilder()
+                        .WithColor(52, 231, 231)
+                        .WithTitle($"Mé jádro úspěšně vymazalo z existence **{count}** zpráv" + 
+                                   (count == 1 ? "u" : Enumerable.Range(1, 4).Contains(count) ? "y" : "")).Build(), 
                     timeout: TimeSpan.FromSeconds(5));
             }
 
@@ -39,7 +49,7 @@ namespace NOVAxis.Modules
                 await ReplyAsync(embed: new EmbedBuilder()
                     .WithColor(220, 20, 60)
                     .WithDescription($"({e.Message})")
-                    .WithTitle($"Mé jádro nebylo schopno vymazat daný počet zpráv").Build());
+                    .WithTitle("Mé jádro nebylo schopno vymazat daný počet zpráv").Build());
             }
         }
 
