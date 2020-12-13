9c4ab7b3b76489b2cacabf642ad42080454cba35
Kryštof Novosad <novosadkry@seznam.cz>
2020-12-13 19:20:21 +0100

    Move command handling to ModuleHandler

--- NOVAxis/Core/Program.cs
+++ NOVAxis/Core/Program.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 using Microsoft.Extensions.DependencyInjection;
 
+using NOVAxis.Modules;
 using NOVAxis.Services.Audio;
 using NOVAxis.Services.Guild;
 using NOVAxis.Services.Database;
@@ -19,12 +20,12 @@ using Victoria;
 namespace NOVAxis.Core
 {
     public class Program
-    {  
-        private static CommandService _commandService;
-        public static IServiceProvider Services;
-
+    {
         public static DiscordShardedClient Client { get; private set; }
         public static ProgramConfig Config { get; private set; }
+
+        public static ModuleHandler Modules { get; private set; }
+        public static IServiceProvider Services { get; private set; }
 
         public static short ShardsReady { get; private set; }
         public static ulong OwnerId => 269182357704015873L;
@@ -50,14 +51,14 @@ namespace NOVAxis.Core
                 MessageCacheSize = 100
             });
 
-            _commandService = new CommandService(new CommandServiceConfig
+            var commandService = new CommandService(new CommandServiceConfig
             {
                 CaseSensitiveCommands = false,
                 DefaultRunMode = RunMode.Async,
                 LogLevel = Config.Log.Severity
             });
 
-            LavaConfig lavaConfig = new LavaConfig
+            var lavaConfig = new LavaConfig
             {
                 Hostname = Config.Lavalink.Host,
                 Port = Config.Lavalink.Port,
@@ -66,14 +67,14 @@ namespace NOVAxis.Core
                 LogSeverity = Config.Log.Severity
             };
 
-            LavaNode lavaNode = new LavaNode(Client, lavaConfig);
+            var lavaNode = new LavaNode(Client, lavaConfig);
             lavaNode.OnLog += Client_Log;
 
-            DatabaseService databaseService = DatabaseService.GetService(Config.Database);
+            var databaseService = DatabaseService.GetService(Config.Database);
             databaseService.LogEvent += Client_Log;
             if (Config.Database.Active) await databaseService.Setup();
 
-            GuildService guildService = new GuildService(databaseService);
+            var guildService = new GuildService(databaseService);
             if (Config.Database.Active) await guildService.LoadFromDatabase();
 
             Services = new ServiceCollection()
@@ -84,11 +85,10 @@ namespace NOVAxis.Core
                 .AddSingleton(new AudioModuleService(lavaNode))
                 .BuildServiceProvider();
 
-            _commandService.CommandExecuted += CommandService_CommandExecuted;
-            _commandService.AddTypeReader(typeof(TimeSpan), new TypeReaders.AudioModuleTypeReader());
-            await _commandService.AddModulesAsync(Assembly.GetEntryAssembly(), Services);
-            
-            Client.MessageReceived += Client_MessageReceived;
+            Modules = new ModuleHandler(Client, Services, commandService);
+            Modules.LogEvent += Client_Log;
+            await Modules.Setup();
+
             Client.ShardReady += Client_Ready;
             Client.Log += Client_Log;
 
@@ -174,110 +174,6 @@ namespace NOVAxis.Core
             {
                 await Client_Log(new LogMessage(LogSeverity.Info, "Victoria", "Connecting"));
                 _ = lavaNodeInstance.ConnectAsync();
-            }
-        }
-
-        private static async Task Client_MessageReceived(SocketMessage arg)
-        {
-            SocketUserMessage message = (SocketUserMessage)arg;
-            ShardedCommandContext context = new ShardedCommandContext(Client, message);
-
-            if (context.Message == null) return;
-            if (context.User.IsBot) return;
-
-            var guildService = Services.GetService<GuildService>();
-            var guildInfo = await guildService.GetInfo(context);
-
-            string prefix = guildInfo.Prefix;
-
-            int argPos = 0;
-            if (!(context.Message.HasStringPrefix(prefix, ref argPos) || 
-                context.Message.HasMentionPrefix(Client.CurrentUser, ref argPos)))
-                return;
-
-            switch (Client.Status)
-            {
-                case UserStatus.DoNotDisturb:
-                    await context.Channel.SendMessageAsync(embed: new EmbedBuilder()
-                        .WithColor(220, 20, 60)
-                        .WithDescription("(Bot je offline)")
-                        .WithTitle("Mé jádro se nyní nachází ve fázi opravy či restartu").Build());
-                    return;
-
-                case UserStatus.AFK:
-                    await context.Channel.SendMessageAsync(embed: new EmbedBuilder()
-                        .WithColor(255, 150, 0)
-                        .WithDescription("(Bot je nedostupný)")
-                        .WithTitle("Mé jádro se nyní nachází ve fázi rapidního ochlazování").Build());
-                    return;
-            }
-
-            await _commandService.ExecuteAsync(context, argPos, Services);
-        }
-
-        private static async Task CommandService_CommandExecuted(Optional<CommandInfo> info, ICommandContext context, IResult result)
-        {
-            if (!result.IsSuccess)
-            {
-                bool logWarning = false;
-
-                switch (result.Error)
-                {
-                    case CommandError.UnknownCommand:
-                        await context.Channel.SendMessageAsync(embed: new EmbedBuilder()
-                            .WithColor(220, 20, 60)
-                            .WithDescription($"(Příkaz `{context.Message.Content.Split(' ')[0]}` neexistuje)")
-                            .WithTitle("Má verze jádra ještě není schopna téhle funkce").Build());
-                        break;
-
-                    case CommandError.BadArgCount:
-                        await context.Channel.SendMessageAsync(embed: new EmbedBuilder()
-                            .WithColor(220, 20, 60)
-                            .WithDescription("(Neplatný počet argumentů)")
-                            .WithTitle("Počet elektronů v elektronovém obale není roven počtu protonů v atomovém jádře").Build());
-                        break;
-
-                    case CommandError.ObjectNotFound:
-                    case CommandError.ParseFailed:
-                        await context.Channel.SendMessageAsync(embed: new EmbedBuilder()
-                            .WithColor(220, 20, 60)
-                            .WithDescription("(Neplatný argument)")
-                            .WithTitle("Má databáze nebyla schopna rozpoznat daný prvek").Build());
-                        break;
-
-                    case CommandError.UnmetPrecondition:
-                        switch (result.ErrorReason)
-                        {
-                            case "Invalid context for command":
-                                await context.Channel.SendMessageAsync(embed: new EmbedBuilder()
-                                    .WithColor(220, 20, 60)
-                                    .WithDescription("(Přístup odepřen)")
-                                    .WithTitle("Tento příkaz nelze vyvolat přímou zprávou").Build());
-                                break;
-                            case "User has command on cooldown":
-                                await context.Channel.SendMessageAsync(embed: new EmbedBuilder()
-                                    .WithColor(220, 20, 60)
-                                    .WithDescription("(Příkaz je časově omezen)")
-                                    .WithTitle("Mé jádro nyní ochlazuje vybraný modul").Build());
-                                break;
-                            default:
-                                await context.Channel.SendMessageAsync(embed: new EmbedBuilder()
-                                    .WithColor(220, 20, 60)
-                                    .WithDescription("(Přístup odepřen)")
-                                    .WithTitle("Pro operaci s tímto modulem nemáš dodatečnou kvalifikaci").Build());
-                                break;
-                        }
-                        break;
-
-                    default:
-                        logWarning = true;
-                        break;
-                }
-
-                await Client_Log(new LogMessage(
-                    logWarning ? LogSeverity.Warning : LogSeverity.Verbose,
-                    "Command",
-                    $"User {context.User.Username}#{context.User.Discriminator} was unable to execute command '{context.Message.Content}' Reason: '{result.ErrorReason}'"));
             }
         }
     }
---
+++ NOVAxis/Modules/ModuleHandler.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using NOVAxis.Services.Guild;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace NOVAxis.Modules
+{
+    public class ModuleHandler
+    {
+        private readonly DiscordShardedClient _client;
+        private readonly IServiceProvider _services;
+
+        public CommandService CommandService { get; }
+        public event Func<LogMessage, Task> LogEvent;
+
+        public ModuleHandler(DiscordShardedClient client, IServiceProvider services, CommandService commandService)
+        {
+            CommandService = commandService;
+            _services = services;
+            _client = client;
+
+            _client.MessageReceived += MessageReceived;
+            CommandService.CommandExecuted += CommandExecuted;
+        }
+
+        ~ModuleHandler()
+        {
+            _client.MessageReceived -= MessageReceived;
+            CommandService.CommandExecuted -= CommandExecuted;
+        }
+
+        public async Task Setup()
+        {
+            await CommandService.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            CommandService.AddTypeReader(typeof(TimeSpan), new TypeReaders.AudioModuleTypeReader());
+        }
+
+        private async Task MessageReceived(SocketMessage arg)
+        {
+            SocketUserMessage message = (SocketUserMessage)arg;
+            ShardedCommandContext context = new ShardedCommandContext(_client, message);
+
+            if (context.Message == null) return;
+            if (context.User.IsBot) return;
+
+            var guildService = _services.GetService<GuildService>();
+            var guildInfo = await guildService.GetInfo(context);
+
+            string prefix = guildInfo.Prefix;
+
+            int argPos = 0;
+            if (!(context.Message.HasStringPrefix(prefix, ref argPos) ||
+                context.Message.HasMentionPrefix(_client.CurrentUser, ref argPos)))
+                return;
+
+            switch (_client.Status)
+            {
+                case UserStatus.DoNotDisturb:
+                    await context.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                        .WithColor(220, 20, 60)
+                        .WithDescription("(Bot je offline)")
+                        .WithTitle("Mé jádro se nyní nachází ve fázi opravy či restartu").Build());
+                    return;
+
+                case UserStatus.AFK:
+                    await context.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                        .WithColor(255, 150, 0)
+                        .WithDescription("(Bot je nedostupný)")
+                        .WithTitle("Mé jádro se nyní nachází ve fázi rapidního ochlazování").Build());
+                    return;
+            }
+
+            await CommandService.ExecuteAsync(context, argPos, _services);
+        }
+
+        private async Task CommandExecuted(Optional<CommandInfo> info, ICommandContext context, IResult result)
+        {
+            if (!result.IsSuccess)
+            {
+                string title = string.Empty; 
+                string description = string.Empty;
+
+                bool logWarning = false;
+
+                switch (result.Error)
+                {
+                    case CommandError.UnknownCommand:
+                        title = "Má verze jádra ještě není schopna téhle funkce";
+                        description = $"(Příkaz `{context.Message.Content.Split(' ')[0]}` neexistuje)";
+                        break;
+
+                    case CommandError.BadArgCount:
+                        title = "Počet elektronů v elektronovém obale není roven počtu protonů v atomovém jádře";
+                        description = "(Neplatný počet argumentů)";
+                        break;
+
+                    case CommandError.ObjectNotFound:
+                    case CommandError.ParseFailed:
+                        title = "Má databáze nebyla schopna rozpoznat daný prvek";
+                        description = "(Neplatný argument)";
+                        break;
+
+                    case CommandError.UnmetPrecondition:
+                        switch (result.ErrorReason)
+                        {
+                            case "Invalid context for command":
+                                title = "Tento příkaz nelze vyvolat přímou zprávou";
+                                description = "(Přístup odepřen)";
+                                break;
+                            case "User has command on cooldown":
+                                title = "Mé jádro nyní ochlazuje vybraný modul";
+                                description = "(Příkaz je časově omezen)";
+                                break;
+                            default:
+                                title = "Pro operaci s tímto modulem nemáš dodatečnou kvalifikaci";
+                                description = "(Přístup odepřen)";
+                                break;
+                        }
+                        break;
+
+                    default:
+                        logWarning = true;
+                        break;
+                }
+
+                if (!string.IsNullOrEmpty(title) && !string.IsNullOrEmpty(description))
+                {
+                    await context.Channel.SendMessageAsync(embed: new EmbedBuilder()
+                        .WithColor(220, 20, 60)
+                        .WithDescription(description)
+                        .WithTitle(title).Build());
+                }
+
+                LogEvent?.Invoke(new LogMessage(
+                    logWarning ? LogSeverity.Warning : LogSeverity.Verbose,
+                    "Command",
+                    $"User {context.User.Username}#{context.User.Discriminator} was unable to execute command '{context.Message.Content}' Reason: '{result.ErrorReason}'"));
+            }
+        }
+    }
+}
