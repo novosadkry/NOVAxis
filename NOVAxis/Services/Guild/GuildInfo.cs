b2097b6dc69c63411487d0a41a1bf6cf44727ce2
Kryštof Novosad <novosadkry@seznam.cz>
2020-12-15 10:41:54 +0100

    Move GuildInfo declaration into a separate file

---
+++ NOVAxis/Services/Guild/GuildInfo.cs
@@ -0,0 +1,13 @@
+﻿using NOVAxis.Core;
+
+namespace NOVAxis.Services.Guild
+{
+    public struct GuildInfo
+    {
+        public string Prefix { get; set; }
+        public ulong DjRole { get; set; }
+
+        public static GuildInfo Default => 
+            new GuildInfo {Prefix = Program.Config.DefaultPrefix};
+    }
+}
