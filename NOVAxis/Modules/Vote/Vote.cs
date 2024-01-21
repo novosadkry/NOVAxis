242ead5498c7d6a0479b3bb173de705234d54a89
Kryštof Novosad <novosadkry@seznam.cz>
2024-01-21 13:02:56 +0100

    Implement VoteModule module

---
+++ NOVAxis/Modules/Vote/Vote.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Discord;
+using Discord.Interactions;
+
+namespace NOVAxis.Modules.Vote
+{
+    public readonly record struct Vote(IGuildUser User, int OptionIndex);
+
+    public class VoteContext
+    {
+        public VoteContext(StartVoteModal modal)
+        {
+            Votes = [];
+            Subject = modal.Subject;
+            Options = modal.Options.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public string Subject { get; }
+        public string[] Options { get; }
+        public List<Vote> Votes { get; }
+    }
+
+    public class StartVoteModal : IModal
+    {
+        public string Title => "Nové hlasování";
+
+        [RequiredInput]
+        [InputLabel("Zadejte téma pro nové hlasování")]
+        [ModalTextInput("subject", maxLength: 100)]
+        public string Subject { get; set; }
+
+        [RequiredInput]
+        [InputLabel("Možnosti (každá na novém řádku)")]
+        [ModalTextInput("options", maxLength: 200, initValue: "Ano\nNe", style: TextInputStyle.Paragraph)]
+        public string Options { get; set; }
+    }
+}
