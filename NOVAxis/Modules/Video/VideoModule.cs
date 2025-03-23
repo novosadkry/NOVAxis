1b5b6ef0fad18b237668dced19e8275ecd4a0f44
Kryštof Novosad <novosadkry@seznam.cz>
2025-03-23 13:13:19 +0100

    Add yt-dlp wrapper classes

---
+++ NOVAxis/Modules/Video/VideoModule.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+using Discord.Interactions;
+
+using NOVAxis.Preconditions;
+
+namespace NOVAxis.Modules.Video
+{
+    [RequireContext(ContextType.Guild)]
+    public class VideoModule : InteractionModuleBase<ShardedInteractionContext>
+    {
+        [Cooldown(30)]
+        [SlashCommand("download", "Downloads a given video")]
+        public async Task Download(string url)
+        {
+
+        }
+    }
+}
