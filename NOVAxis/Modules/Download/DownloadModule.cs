dd16037296954f72b4a7f9fd46e5be563b41aa04
Kryštof Novosad <novosadkry@seznam.cz>
2025-03-27 12:08:49 +0100

    Download refactor and error handling

--- NOVAxis/Modules/Download/DownloadModule.cs
+++ NOVAxis/Modules/Download/DownloadModule.cs
@@ -20,6 +20,12 @@ namespace NOVAxis.Modules.Download
     [Group("download", "Various commands for video and audio processing")]
     public class DownloadModule : InteractionModuleBase<ShardedInteractionContext>
     {
+        private enum DownloadType
+        {
+            Video,
+            Audio
+        }
+
         public DownloadService DownloadService { get; set; }
         public WebServerService WebServerService { get; set; }
         public InteractionCache InteractionCache { get; set; }
@@ -27,69 +33,88 @@ namespace NOVAxis.Modules.Download
         [SlashCommand("video", "Downloads a given video")]
         public async Task Video(string url)
         {
-            await DeferAsync(ephemeral: true);
-
-            var metadata = await DownloadService.DownloadVideoMetadata(url);
-            var id = InteractionCache.Store(metadata);
-
-            var selectMenuOptions = metadata.Formats
-                .Select(f => new SelectMenuOptionBuilder()
-                    .WithLabel(f.ToString())
-                    .WithValue(f.FormatId))
-                .Reverse()
-                .ToList();
-
-            await FollowupAsync(
-                ephemeral: true,
-                components: new ComponentBuilder()
-                    .WithSelectMenu(
-                        $"download_video_format_select_{id}",
-                        selectMenuOptions,
-                        "Vyber formát, ve kterém chceš video stáhnout.")
-                    .Build(),
-                embed: new EmbedBuilder()
-                    .WithColor(52, 231, 231)
-                    .WithTitle(metadata.Title)
-                    .WithUrl(metadata.WebpageUrl)
-                    .WithThumbnailUrl(metadata.Thumbnail)
-                    .Build());
+            await HandleDownloadRequest(url, DownloadType.Video);
         }
 
         [SlashCommand("audio", "Downloads a given audio")]
         public async Task Audio(string url)
         {
-            await DeferAsync(ephemeral: true);
-
-            var metadata = await DownloadService.DownloadVideoMetadata(url);
-            var id = InteractionCache.Store(metadata);
-
-            var selectMenuOptions = Enum.GetValues<AudioConversionFormat>()
-                .Select(f => new SelectMenuOptionBuilder()
-                    .WithLabel(f.ToString())
-                    .WithValue(((int)f).ToString()))
-                .ToList();
-
-            await FollowupAsync(
-                ephemeral: true,
-                components: new ComponentBuilder()
-                    .WithSelectMenu(
-                        $"download_audio_format_select_{id}",
-                        selectMenuOptions,
-                        "Vyber formát, ve kterém chceš audio stáhnout.")
-                    .Build(),
-                embed: new EmbedBuilder()
-                    .WithColor(52, 231, 231)
-                    .WithTitle(metadata.Title)
-                    .WithUrl(metadata.WebpageUrl)
-                    .WithThumbnailUrl(metadata.Thumbnail)
-                    .Build());
+            await HandleDownloadRequest(url, DownloadType.Audio);
         }
 
-        [ComponentInteraction("download_video_format_select_*", true)]
-        public async Task VideoFormatSelect(ulong interactionId, string selectedFormat)
+        private async Task HandleDownloadRequest(string url, DownloadType downloadType)
         {
             await DeferAsync(ephemeral: true);
+
+            try
+            {
+                var metadata = await DownloadService.DownloadVideoMetadata(url);
+                var id = InteractionCache.Store(metadata);
+                var downloadTypeString = downloadType.ToString().ToLower();
+
+                var selectMenuOptions = downloadType switch
+                {
+                    DownloadType.Video => metadata.Formats
+                        .Select(f => new SelectMenuOptionBuilder()
+                            .WithLabel(f.ToString())
+                            .WithValue(f.FormatId))
+                        .Reverse()
+                        .ToList(),
+
+                    DownloadType.Audio => Enum.GetValues<AudioConversionFormat>()
+                        .Select(f => new SelectMenuOptionBuilder()
+                            .WithLabel(f.ToString())
+                            .WithValue(((int)f).ToString()))
+                        .ToList(),
+
+                    _ => throw new ArgumentException("Invalid download type")
+                };
+
+                await FollowupAsync(
+                    ephemeral: true,
+                    components: new ComponentBuilder()
+                        .WithSelectMenu(
+                            $"download_format_select_{id},{downloadTypeString}",
+                            selectMenuOptions,
+                            $"Vyber formát, ve kterém chceš {downloadTypeString} stáhnout.")
+                        .Build(),
+                    embed: new EmbedBuilder()
+                        .WithColor(52, 231, 231)
+                        .WithTitle(metadata.Title)
+                        .WithUrl(metadata.WebpageUrl)
+                        .WithThumbnailUrl(metadata.Thumbnail)
+                        .Build());
+            }
+            catch (DownloadException e)
+            {
+                await FollowupAsync(
+                    embed: new EmbedBuilder()
+                        .WithColor(220, 20, 60)
+                        .WithAuthor("Při načítání došlo k chybě.")
+                        .WithDescription($"```{string.Join('\n', e.ErrorOutput)}```")
+                        .Build());
+            }
+        }
+
+        [ComponentInteraction("download_format_select_*,*", true)]
+        public async Task VideoFormatSelect(ulong interactionId, string downloadTypeString, string selectedFormat)
+        {
+            var downloadType = downloadTypeString switch
+            {
+                "video" => DownloadType.Video,
+                "audio" => DownloadType.Audio,
+                _ => throw new ArgumentException("Invalid download type")
+            };
+
+            await HandleFormatSelect(interactionId, downloadType, selectedFormat);
+        }
+
+        private async Task HandleFormatSelect(ulong interactionId, DownloadType downloadType, string selectedFormat)
+        {
+            await DeferAsync(ephemeral: true);
+
             var interaction = (IComponentInteraction)Context.Interaction;
+            var downloadTypeString = downloadType.ToString().ToLower();
 
             if (InteractionCache[interactionId] is not VideoData metadata)
             {
@@ -108,65 +133,52 @@ namespace NOVAxis.Modules.Download
                     .WithColor(52, 231, 231)
                     .WithTitle(metadata.Title)
                     .WithUrl(metadata.WebpageUrl)
-                    .WithDescription("Až bude video připravené, pošlu ti odkaz ke stažení.")
+                    .WithDescription($"Až bude {downloadTypeString} připravené, pošlu ti odkaz ke stažení.")
                     .WithThumbnailUrl(metadata.Thumbnail)
                     .Build());
 
-            var format = metadata.Formats.FirstOrDefault(f => f.FormatId == selectedFormat);
-            var uuid = await DownloadService.DownloadVideo(Context.User, metadata.WebpageUrl, selectedFormat);
-            var uri = await WebServerService.ServeDownload(uuid);
-
-            await Context.User.SendMessageAsync(
-                embed: new EmbedBuilder()
-                    .WithColor(52, 231, 231)
-                    .WithTitle(metadata.Title)
-                    .WithUrl(uri)
-                    .WithAuthor("Tvoje video je připravené k stažení.")
-                    .WithDescription(format?.ToString())
-                    .WithThumbnailUrl(metadata.Thumbnail)
-                    .Build());
-        }
-
-        [ComponentInteraction("download_audio_format_select_*", true)]
-        public async Task AudioFormatSelect(ulong interactionId, string selectedFormat)
-        {
-            await DeferAsync(ephemeral: true);
-            var interaction = (IComponentInteraction)Context.Interaction;
-
-            if (InteractionCache[interactionId] is not VideoData metadata)
+            try
             {
-                await FollowupAsync(ephemeral: true, embed: new EmbedBuilder()
-                    .WithColor(220, 20, 60)
-                    .WithDescription("(Vypršel časový limit)")
-                    .WithTitle("Mé jádro přerušilo čekání na lidský vstup")
-                    .Build());
+                Guid uuid;
+                string description;
 
-                return;
+                if (downloadType == DownloadType.Video)
+                {
+                    var format = metadata.Formats.FirstOrDefault(f => f.FormatId == selectedFormat);
+                    uuid = await DownloadService.DownloadVideo(Context.User, metadata.WebpageUrl, selectedFormat);
+                    description = format?.ToString();
+                }
+                else
+                {
+                    var format = Enum.Parse<AudioConversionFormat>(selectedFormat);
+                    uuid = await DownloadService.DownloadAudio(Context.User, metadata.WebpageUrl, format);
+                    description = format.ToString();
+                }
+
+                var uri = await WebServerService.ServeDownload(uuid);
+
+                await Context.User.SendMessageAsync(
+                    embed: new EmbedBuilder()
+                        .WithColor(52, 231, 231)
+                        .WithTitle(metadata.Title)
+                        .WithUrl(uri)
+                        .WithAuthor($"Tvoje {downloadTypeString} je připravené ke stažení.")
+                        .WithDescription(description)
+                        .WithThumbnailUrl(metadata.Thumbnail)
+                        .Build());
             }
-
-            await interaction.FollowupAsync(
-                ephemeral: true,
-                embed: new EmbedBuilder()
-                    .WithColor(52, 231, 231)
-                    .WithTitle(metadata.Title)
-                    .WithUrl(metadata.WebpageUrl)
-                    .WithDescription("Až bude audio připravené, pošlu ti odkaz ke stažení.")
-                    .WithThumbnailUrl(metadata.Thumbnail)
-                    .Build());
-
-            var format = Enum.Parse<AudioConversionFormat>(selectedFormat);
-            var uuid = await DownloadService.DownloadAudio(Context.User, metadata.WebpageUrl, format);
-            var uri = await WebServerService.ServeDownload(uuid);
-
-            await Context.User.SendMessageAsync(
-                embed: new EmbedBuilder()
-                    .WithColor(52, 231, 231)
-                    .WithTitle(metadata.Title)
-                    .WithUrl(uri)
-                    .WithAuthor("Tvoje audio je připravené k stažení.")
-                    .WithDescription(format.ToString())
-                    .WithThumbnailUrl(metadata.Thumbnail)
-                    .Build());
+            catch (DownloadException e)
+            {
+                await Context.User.SendMessageAsync(
+                    embed: new EmbedBuilder()
+                        .WithColor(220, 20, 60)
+                        .WithTitle(metadata.Title)
+                        .WithUrl(metadata.WebpageUrl)
+                        .WithAuthor("Při stahování došlo k chybě.")
+                        .WithDescription($"```{string.Join('\n', e.ErrorOutput)}```")
+                        .WithThumbnailUrl(metadata.Thumbnail)
+                        .Build());
+            }
         }
     }
 }
